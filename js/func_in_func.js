5172183e15cf86ebcf15cb808ab988950035e466
Amanda Rodgers <aarodgerswork@gmail.com>
2022-05-16 11:16:04 -0400

    Added closure.html

--- js/func_in_func.js
+++ js/func_in_func.js
@@ -16,11 +16,12 @@ document.write(personalGreeting.toString());
   * the local variables within it are inaccessible, but by returning a function reference (sayWelcome)
   * the greetVisitor function's internal data becomes accessible to the outside world.
   *The key to understanding closures (is a local variable for a function, kept alive after the function
-    has returned) are understanding variable scope in JS and understanding the diff btwn executing
+    has returned, it includes the code of the returned inner function) are understanding variable scope in JS and understanding the diff btwn executing
     a function and a function reference. By assigning the return value of the greetVisitor function to
     the new personalGreeting function, the program stores the code of the sayWelcome function.
     You can test this by using the toString() method: personalGreeting.toString()
 
     The variable welcome is a copy of the variable welcome from the original greetVisitor function
     at the time when the closure was created.
+
   */
